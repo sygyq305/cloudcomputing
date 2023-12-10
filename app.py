75a8d35e9c937e057d8253b666897fb1e2bf40cf
高宇奇 <gaoyuq305@163.com>
2023-12-10 16:38:36 +0800

    修改assignment1

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
 import os
-
+import pandas as pd
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
 
 app = Flask(__name__)
-
+# 从 CSV 文件加载评论
+reviews_data = pd.read_csv('amazon-reviews.csv')
+cities_data = pd.read_csv('us-cities.csv')
 
 @app.route('/')
-def index():
-   print('Request for index page received')
-   return render_template('index.html')
+# def index():
+#    print('Request for index page received')
+#    return render_template('index.html')
+def show_reviews():
+    score_path = {1: 'score-1', 2: 'score-2', 3: 'score-3', 4: 'score-4', 5: 'score-5'}
+
+    return render_template('index.html', reviews=reviews_data.to_dict('records'), score_path=score_path)
+
+
+@app.route('/city_details/<city>')
+def city_details(city):
+    # 获取城市详情数据
+    city_details = cities_data[cities_data['city'] == city].to_dict(orient='records')
+    if not city_details:
+        return 'City details not found'
+
+    return render_template('city_details.html', city_details=city_details[0])
+
 
 @app.route('/favicon.ico')
 def favicon():
